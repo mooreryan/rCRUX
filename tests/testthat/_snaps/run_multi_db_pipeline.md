d066f13619e7085b670e8cc68edd94589d3c1223
Ryan M. Moore <rmm1047@gmail.com>
2024-08-19 15:25:55 -0400

    Test hits to only a single DB

--- tests/testthat/_snaps/run_multi_db_pipeline.md
+++ tests/testthat/_snaps/run_multi_db_pipeline.md
@@ -16,3 +16,21 @@
        [9] "./16S/db_collated/derep_and_clean_db/Sequences_with_multiple_taxonomic_paths.csv"                
       [10] "./16S/db_collated/derep_and_clean_db/Sequences_with_single_taxonomic_path.csv"                   
 
+# run_multi_db_pipeline works with hits to only a single DB works
+
+    Code
+      files_to_snapshot %>% purrr::map_vec(function(filename) {
+        stringr::str_replace(string = filename, pattern = "^/.*/16S", replacement = "./16S")
+      }) %>% print()
+    Output
+       [1] "./16S/db_collated/blast_seeds_output/Nitrospira.fasta"                                           
+       [2] "./16S/db_collated/blast_seeds_output/Nitrospira_taxonomy.txt"                                    
+       [3] "./16S/db_collated/blast_seeds_output/summary.csv"                                                
+       [4] "./16S/db_collated/derep_and_clean_db/Nitrospira_derep_and_clean.fasta"                           
+       [5] "./16S/db_collated/derep_and_clean_db/Nitrospira_derep_and_clean_taxonomy.txt"                    
+       [6] "./16S/db_collated/derep_and_clean_db/Nitrospira_derep_and_clean_unique_taxonomic_rank_counts.txt"
+       [7] "./16S/db_collated/derep_and_clean_db/Sequences_with_lowest_common_taxonomic_path_agreement.csv"  
+       [8] "./16S/db_collated/derep_and_clean_db/Sequences_with_mostly_NA_taxonomic_paths.csv"               
+       [9] "./16S/db_collated/derep_and_clean_db/Sequences_with_multiple_taxonomic_paths.csv"                
+      [10] "./16S/db_collated/derep_and_clean_db/Sequences_with_single_taxonomic_path.csv"                   
+
--- tests/testthat/test-run_multi_db_pipeline.R
+++ tests/testthat/test-run_multi_db_pipeline.R
@@ -1,3 +1,14 @@
+# Pass in the unique ID and it returns a function that that will call
+# expect_snapshot_file, but with a unique ID appended to the name of the file.
+# Use this to keep the file snapshots unique to the individual test even with a
+# single test file.
+expect_snapshot_file_factory <- function(id) {
+  function(path) {
+    name <- stringr::str_glue("{id}__{basename(path)}")
+    testthat::expect_snapshot_file(path = path, name = name)
+  }
+}
+
 test_that("run_multi_db_pipeline works", {
   output_directory_path_top <- tempfile()
   dir.create(
@@ -62,7 +73,10 @@ test_that("run_multi_db_pipeline works", {
     include.dirs = FALSE,
     all.files = FALSE
   )
-  lapply(files_to_snapshot, expect_snapshot_file)
+  lapply(
+    files_to_snapshot,
+    expect_snapshot_file_factory("ec8bc5bc-89c4-49a2-8ff1-d203f0bbfbc0")
+  )
 
   expect_snapshot({
     files_to_snapshot %>%
@@ -169,7 +183,7 @@ test_that("run_multi_db_pipeline with a single blast DB gives an early error", {
   }
 })
 
-test_that("run_multi_db_pipeline with no hits to any DB", {
+test_that("run_multi_db_pipeline with no hits to any DB fails", {
   output_directory_path_top <- tempfile()
   dir.create(
     output_directory_path_top,
@@ -227,6 +241,92 @@ test_that("run_multi_db_pipeline with no hits to any DB", {
       regexp = "none of the BLAST DBs had any hits"
     )
   )
+
+  # Clean up the output directory
+  if (dir.exists(output_directory_path_top)) {
+    unlink(output_directory_path_top, recursive = TRUE)
+  }
+})
+
+
+test_that("run_multi_db_pipeline works with hits to only a single DB works", {
+  output_directory_path_top <- tempfile()
+  dir.create(
+    output_directory_path_top,
+    recursive = TRUE, mode = "0750"
+  )
+
+
+  output_directory_path <- file.path(
+    output_directory_path_top,
+    "16S"
+  )
+
+  blast_db_path <- system.file(
+    package = "rCRUX", "mock-db/mock-db-sequences-split"
+  )
+
+  blast_db_paths <- paste0(
+    file.path(blast_db_path, "mock-db-sequences.part_00"),
+    c(1, 4)
+  )
+
+  accession_taxa_sql_path <- system.file(
+    package = "rCRUX",
+    "mock-db/taxonomizr-ncbi-db-small.sql"
+  )
+
+  # Primers
+  # Nitrospira F probe, Bacteria 1492R
+  forward_primer_seq <- "AGAGGAGCGCGGAATTCC"
+  reverse_primer_seq <- "TACCTTGTTACGACTT"
+
+  metabarcode_name <- "Nitrospira"
+
+  parallel_jobs <- 2
+
+  collated_output_dir <- run_multi_db_pipeline(
+    forward_primer_seq = forward_primer_seq,
+    reverse_primer_seq = reverse_primer_seq,
+    metabarcode_name = metabarcode_name,
+    output_directory_path = output_directory_path,
+    accession_taxa_sql_path = accession_taxa_sql_path,
+    blast_db_paths = blast_db_paths,
+    parallel_jobs = parallel_jobs,
+    args_get_seeds_local_multi_db = list(
+      minimum_length = 5,
+      maximum_length = 900,
+      num_threads = 1
+    ),
+    args_blast_seeds_multi_db = list(
+      expand_vectors = TRUE,
+      minimum_length = 5,
+      maximum_length = 900,
+      num_threads = 1
+    )
+  )
+
+  files_to_snapshot <- list.files(
+    path = collated_output_dir,
+    full.names = TRUE,
+    recursive = TRUE,
+    include.dirs = FALSE,
+    all.files = FALSE
+  )
+  lapply(
+    files_to_snapshot,
+    expect_snapshot_file_factory("94e25cf9-fa25-4acd-849f-ac1f5923c7fe")
+  )
+
+  expect_snapshot({
+    files_to_snapshot %>%
+      purrr::map_vec(function(filename) {
+        stringr::str_replace(
+          string = filename, pattern = "^/.*/16S", replacement = "./16S"
+        )
+      }) %>%
+      print()
+  })
 
   # Clean up the output directory
   if (dir.exists(output_directory_path_top)) {
