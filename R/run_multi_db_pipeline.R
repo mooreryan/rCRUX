810baae28f1fca2a303acbe84f74832d8aa130ce
Ryan M. Moore <rmm1047@gmail.com>
2024-08-19 13:50:45 -0400

    Handle the case where none of the BLAST DBs had any hits

--- R/run_multi_db_pipeline.R
+++ R/run_multi_db_pipeline.R
@@ -40,6 +40,11 @@ run_multi_db_pipeline <- function(
   seeds_tbl <- result$collated_table
   blast_db_paths <- result$blast_db_paths_with_hits
 
+  # If none of the BLAST DBs found any seeds, then fail.
+  if (length(blast_db_paths) == 0) {
+    stop("When searching for seeds, none of the BLAST DBs had any hits.")
+  }
+
   # Write the seeds_tbl CSV to a temporary file.
   seeds_tbl_path <- tempfile()
   readr::write_csv(x = seeds_tbl, file = seeds_tbl_path)
--- tests/testthat/test-run_multi_db_pipeline.R
+++ tests/testthat/test-run_multi_db_pipeline.R
@@ -168,3 +168,68 @@ test_that("run_multi_db_pipeline with a single blast DB gives an early error", {
     unlink(output_directory_path_top, recursive = TRUE)
   }
 })
+
+test_that("run_multi_db_pipeline with no hits to any DB", {
+  output_directory_path_top <- tempfile()
+  dir.create(
+    output_directory_path_top,
+    recursive = TRUE, mode = "0750"
+  )
+
+
+  output_directory_path <- file.path(
+    output_directory_path_top,
+    "16S"
+  )
+
+  blast_db_path <- system.file(
+    package = "rCRUX", "mock-db/mock-db-sequences-split"
+  )
+
+  blast_db_paths <- paste0(
+    file.path(blast_db_path, "mock-db-sequences.part_00"),
+    1:4
+  )
+
+  accession_taxa_sql_path <- system.file(
+    package = "rCRUX",
+    "mock-db/taxonomizr-ncbi-db-small.sql"
+  )
+
+  # Primers
+  forward_primer_seq <- "GTGTGTGTGTGTGTGTGT"
+  reverse_primer_seq <- "GTGTGTGTGTGTGTGT"
+
+  metabarcode_name <- "Nitrospira"
+
+  parallel_jobs <- 2
+
+  expect_error(
+    run_multi_db_pipeline(
+      forward_primer_seq = forward_primer_seq,
+      reverse_primer_seq = reverse_primer_seq,
+      metabarcode_name = metabarcode_name,
+      output_directory_path = output_directory_path,
+      accession_taxa_sql_path = accession_taxa_sql_path,
+      blast_db_paths = blast_db_paths,
+      parallel_jobs = parallel_jobs,
+      args_get_seeds_local_multi_db = list(
+        minimum_length = 5,
+        maximum_length = 900,
+        num_threads = 1
+      ),
+      args_blast_seeds_multi_db = list(
+        expand_vectors = TRUE,
+        minimum_length = 5,
+        maximum_length = 900,
+        num_threads = 1
+      ),
+      regexp = "none of the BLAST DBs had any hits"
+    )
+  )
+
+  # Clean up the output directory
+  if (dir.exists(output_directory_path_top)) {
+    unlink(output_directory_path_top, recursive = TRUE)
+  }
+})
