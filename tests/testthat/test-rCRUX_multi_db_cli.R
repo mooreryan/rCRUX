988bcd573f709a422ec20d5f57749ef3c0c0697f
Ryan M. Moore <rmm1047@gmail.com>
2024-09-06 11:34:31 -0400

    Reorg

--- tests/testthat/test-rCRUX_multi_db_cli.R
+++ tests/testthat/test-rCRUX_multi_db_cli.R
@@ -1,4 +1,4 @@
-test_that("rCRUX_multi_db.R CLI program works", {
+set_up <- function() {
   output_directory_path_top <- tempfile()
   dir.create(
     output_directory_path_top,
@@ -8,6 +8,19 @@ test_that("rCRUX_multi_db.R CLI program works", {
     output_directory_path_top,
     "16S"
   )
+
+  blast_db_paths <- sapply(1:4, function(n) {
+    file.path(
+      system.file(
+        package = "rCRUX",
+        file.path(
+          "mock-db",
+          "mock-db-sequences-split"
+        )
+      ),
+      stringr::str_glue("mock-db-sequences.part_00{n}")
+    )
+  })
 
   # These are the params that will be turned into a yaml file.
   params <- list(
@@ -20,18 +33,7 @@ test_that("rCRUX_multi_db.R CLI program works", {
       file.path("mock-db", "taxonomizr-ncbi-db-small.sql")
     ),
     parallel_jobs = 2,
-    blast_db_paths = sapply(1:4, function(n) {
-      file.path(
-        system.file(
-          package = "rCRUX",
-          file.path(
-            "mock-db",
-            "mock-db-sequences-split"
-          )
-        ),
-        stringr::str_glue("mock-db-sequences.part_00{n}")
-      )
-    }),
+    blast_db_paths = blast_db_paths,
     args_get_seeds_local_multi_db = list(
       minimum_length = 5,
       maximum_length = 900,
@@ -45,9 +47,23 @@ test_that("rCRUX_multi_db.R CLI program works", {
     )
   )
 
-  # Write the params.
   params_yml_path <- tempfile()
-  yaml::write_yaml(params, params_yml_path)
+
+  rcrux_package_path <- system.file(package = "rCRUX", "..")
+
+  list(
+    output_directory_path = output_directory_path,
+    params = params,
+    params_yml_path = params_yml_path,
+    rcrux_package_path = rcrux_package_path
+  )
+}
+
+test_that("rCRUX_multi_db.R CLI program works", {
+  conf <- set_up()
+
+  # Write the params.
+  yaml::write_yaml(conf$params, conf$params_yml_path)
 
   # This is the CLI script.
   cli_script_file_path <- system.file(
@@ -56,7 +72,6 @@ test_that("rCRUX_multi_db.R CLI program works", {
     "rCRUX_multi_db.R"
   )
 
-  rcrux_package_path <- system.file(package = "rCRUX", "..")
 
   # Actually run the CLI program.
   system2(
@@ -64,14 +79,14 @@ test_that("rCRUX_multi_db.R CLI program works", {
     args = c(
       "--vanilla",
       cli_script_file_path,
-      params_yml_path,
-      rcrux_package_path
+      conf$params_yml_path,
+      conf$rcrux_package_path
     )
   )
 
   # Then snapshot the files.
   files_to_snapshot <- list.files(
-    path = output_directory_path,
+    path = conf$output_directory_path,
     full.names = TRUE,
     recursive = TRUE,
     include.dirs = FALSE,
