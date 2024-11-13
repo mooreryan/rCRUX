34221fbb5f9e8659be261a34032b8659a06c390a
Ryan M. Moore <rmm1047@gmail.com>
2024-11-13 16:36:41 -0500

    Fix the logger

--- R/logging.R
+++ R/logging.R
@@ -1,6 +1,9 @@
 rcrux_logger_namespace <- "rCRUX"
 
 .onLoad <- function(...) {
+  # Use more precision for the timestamps in the logger.
+  options(digits.secs = 6)
+
   # JSON formatter for logger is set on package load because the log message
   # formatter is specific to the way rCRUX does logging calls.
   logger::log_formatter(
@@ -32,13 +35,13 @@ assign_logfile_lock <- function() {
 # to accept a thunk instead.
 
 make_log_function <- function(log_fn) {
-  function(msg, extra) {
+  function(msg, details = NULL) {
     logfile_lock <- get_logfile_lock()
 
     # The user has not specified a lockfile for the log.  So, just log and be
     # done with it.
     if (is.null(logfile_lock)) {
-      log_fn(msg, extra)
+      log_fn(msg = msg, details = details)
       return(invisible(NULL))
     }
 
@@ -48,7 +51,7 @@ make_log_function <- function(log_fn) {
 
     # Then, log while holding the lock.
     # logger::log_level(FYI, ...)
-    log_fn(msg, extra)
+    log_fn(msg = msg, details = details)
 
     # `lock` will be NULL if a timeout occured and it was unable to aquire the
     # lock.
@@ -63,62 +66,59 @@ make_log_function <- function(log_fn) {
   }
 }
 
-# Set up logging facade.
-
 # The default of `.topcall = sys.call(-1)` gives most log lines a `fn` value of
 # `log_fn`.  Setting it to `-2`, gives most log calls the name of the function
 # from which the logging function was called.  However, if the logger is called
 # directly from a script not in a function, it gives NA.  It's good enough for
 # now.
 
-rcrux_log_fatal <- make_log_function(function(msg, details = NA) {
-  logger::log_fatal(
-    msg = msg,
-    details = details,
-    .topcall = sys.call(-2),
-    namespace = rcrux_logger_namespace
-  )
-})
-rcrux_log_error <- make_log_function(function(msg, details = NA) {
-  logger::log_error(
-    msg = msg,
-    details = details,
-    .topcall = sys.call(-2),
-    namespace = rcrux_logger_namespace
-  )
-})
-rcrux_log_warn <- make_log_function(function(msg, details = NA) {
-  logger::log_warn(
-    msg = msg,
-    details = details,
-    .topcall = sys.call(-2),
-    namespace = rcrux_logger_namespace
-  )
-})
-rcrux_log_info <- make_log_function(function(msg, details = NA) {
-  logger::log_info(
-    msg = msg,
-    details = details,
-    .topcall = sys.call(-2),
-    namespace = rcrux_logger_namespace
-  )
-})
-rcrux_log_debug <- make_log_function(function(msg, details = NA) {
-  logger::log_debug(
-    msg = msg,
-    details = details,
-    .topcall = sys.call(-2),
-    namespace = rcrux_logger_namespace
-  )
-})
-rcrux_log_trace <- make_log_function(function(msg, details = NA) {
-  logger::log_trace(
-    msg = msg,
-    details = details,
-    .topcall = sys.call(-2),
-    namespace = rcrux_logger_namespace
-  )
-})
+# The tedious `details` stuff is there to keep the logs looking a little nicer
+# when `details` aren't provided.  Could be there is a nicer way to handle it in
+# `logger` package itself.
+wrap_logger <- function(log_fn) {
+  function(msg, details = NULL) {
+    if (is.null(details)) {
+      log_fn(
+        msg = msg,
+        # .topcall = sys.call(1),
+        namespace = rcrux_logger_namespace
+      )
+    } else {
+      log_fn(
+        msg = msg,
+        details = details,
+        # .topcall = sys.call(1),
+        namespace = rcrux_logger_namespace
+      )
+    }
+  }
+}
+
+# Set up logging facade.
+
+rcrux_log_fatal <- logger::log_fatal %>%
+  wrap_logger() %>%
+  make_log_function()
+
+rcrux_log_error <- logger::log_error %>%
+  wrap_logger() %>%
+  make_log_function()
+
+rcrux_log_warn <- logger::log_warn %>%
+  wrap_logger() %>%
+  make_log_function()
+
+rcrux_log_info <- logger::log_info %>%
+  wrap_logger() %>%
+  make_log_function()
+
+rcrux_log_debug <- logger::log_debug %>%
+  wrap_logger() %>%
+  make_log_function()
+
+rcrux_log_trace <- logger::log_trace %>%
+  wrap_logger() %>%
+  make_log_function()
 
 set_up_logger <- function() {
   logger::log_threshold(logger::INFO, namespace = rcrux_logger_namespace)
@@ -139,7 +139,10 @@ set_up_logger <- function() {
       logger::log_appender(namespace = rcrux_logger_namespace, index = 2)
 
     logger::log_layout(
-      logger::layout_json_parser(),
+      logger::layout_json_parser(fields = c(
+        "time", "level", "ns", "ans", "topenv", "node", "arch",
+        "os_name", "os_release", "os_version", "pid", "user", "r_version"
+      )),
       namespace = rcrux_logger_namespace, index = 2
     )
   }
