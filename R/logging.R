ffcb9ccc52f584b2c4188418a91faa07db87f86c
Ryan M. Moore <rmm1047@gmail.com>
2024-11-13 20:58:03 -0500

    Explicitly specify the context function in the logger
    The `topcall` param provided by the logger library isn't really giving the result that we are looking for.  So, we provide the data manually.

--- R/logging.R
+++ R/logging.R
@@ -35,13 +35,13 @@ assign_logfile_lock <- function() {
 # to accept a thunk instead.
 
 make_log_function <- function(log_fn) {
-  function(msg, details = NULL) {
+  function(msg, fn = NULL, details = NULL) {
     logfile_lock <- get_logfile_lock()
 
     # The user has not specified a lockfile for the log.  So, just log and be
     # done with it.
     if (is.null(logfile_lock)) {
-      log_fn(msg = msg, details = details)
+      log_fn(msg = msg, fn = fn, details = details)
       return(invisible(NULL))
     }
 
@@ -51,7 +51,7 @@ make_log_function <- function(log_fn) {
 
     # Then, log while holding the lock.
     # logger::log_level(FYI, ...)
-    log_fn(msg = msg, details = details)
+    log_fn(msg = msg, fn = fn, details = details)
 
     # `lock` will be NULL if a timeout occured and it was unable to aquire the
     # lock.
@@ -66,33 +66,38 @@ make_log_function <- function(log_fn) {
   }
 }
 
-# The default of `.topcall = sys.call(-1)` gives most log lines a `fn` value of
-# `log_fn`.  Setting it to `-2`, gives most log calls the name of the function
-# from which the logging function was called.  However, if the logger is called
-# directly from a script not in a function, it gives NA.  It's good enough for
-# now.
-
-# The tedious `details` stuff is there to keep the logs looking a little nicer
-# when `details` aren't provided.  Could be there is a nicer way to handle it in
-# `logger` package itself.
+# The NULL checks are to make the console logs look nicer.  Could be there is a
+# nicer way to handle it in `logger` package itself.
 wrap_logger <- function(log_fn) {
-  function(msg, details = NULL) {
-    if (is.null(details)) {
+  function(msg, fn = NULL, details = NULL) {
+    if (is.null(fn) && is.null(details)) {
       log_fn(
         msg = msg,
-        # .topcall = sys.call(1),
+        namespace = rcrux_logger_namespace
+      )
+    } else if (is.null(fn)) {
+      log_fn(
+        msg = msg,
+        details = details,
+        namespace = rcrux_logger_namespace
+      )
+    } else if (is.null(details)) {
+      log_fn(
+        msg = msg,
+        fn = fn,
         namespace = rcrux_logger_namespace
       )
     } else {
       log_fn(
         msg = msg,
+        fn = fn,
         details = details,
-        # .topcall = sys.call(1),
         namespace = rcrux_logger_namespace
       )
     }
   }
 }
+
 
 # Set up logging facade.
 
